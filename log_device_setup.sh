15a5a537a5f0cc598821cfea4adeb9bebaf98fab
Chris Kunde <chriskunde@live.com>
2024-11-12 12:13:39 -0500

    moved xset to xinitrc file

--- log_device_setup.sh
+++ log_device_setup.sh
@@ -16,11 +16,6 @@ sudo apt update -y && sudo apt upgrade -y
 # Install necessary packages
 sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser xserver-xorg-input-libinput -y
 
-# Disable screen blanking and power saving
-xset -dpms
-xset s off
-xset s noblank
-
 # Fix Chromium "exited_cleanly" flag (if needed)
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"["]+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
@@ -34,8 +29,15 @@ sudo sed -i '/MatchIsTouchscreen "on"/a\        Option "CalibrationMatrix" "0 -1
 
 # Configure .xinitrc (using the dynamically created KIOSK_URL)
 cat << EOF > ~/.xinitrc
+# Disable screen blanking and power saving
+xset -dpms
+xset s off
+xset s noblank
+
 xrandr --output DSI-1 --rotate left  # Replace DSI-1 if necessary
+
 chromium-browser --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar --kiosk "\$KIOSK_URL" --check-for-update-interval=31536000 & # Note the escaped \$
+
 openbox
 exec openbox
 EOF
