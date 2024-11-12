b87a20d56d28e81b8c08c921c1ad6ebf40ba8c5a
Chris Kunde <chriskunde@live.com>
2024-11-12 12:37:54 -0500

    bugfix try 2

--- log_device_setup.sh
+++ log_device_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Base URL (edit this if the base URL changes)
+# Base URL
 BASE_URL="http://10.0.36.52:8000/registerDevice"
 
 # Prompt for device name
@@ -12,10 +12,8 @@ KIOSK_URL="${BASE_URL}?device_name=${DEVICE_NAME}"
 # Set KIOSK_URL in the environment
 echo "export KIOSK_URL=\"${KIOSK_URL}\"" >> ~/.bashrc
 
-
 # Enable autologin to command line
 sudo raspi-config nonint do_boot_behaviour B2
-
 
 # Update and upgrade
 sudo apt update -y && sudo apt upgrade -y
@@ -23,38 +21,34 @@ sudo apt update -y && sudo apt upgrade -y
 # Install necessary packages
 sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser xserver-xorg-input-libinput -y
 
-# Fix Chromium "exited_cleanly" flag (if needed)
-# sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-# sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"["]+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-
-
 # Configure libinput for touch rotation
-sudo mkdir -p /etc/X11/xorg.conf.d  # -p creates the directory if it doesn't exist
+sudo mkdir -p /etc/X11/xorg.conf.d
 sudo cp /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/
-sudo sed -i '/MatchIsTouchscreen "on"/a\        Option "CalibrationMatrix" "0 -1 1 1 0 0 0 0 1"' /etc/X11/xorg.conf.d/40-libinput.conf   # 90 degrees left rotation. Change as needed.
+sudo sed -i '/MatchIsTouchscreen "on"/a\        Option "CalibrationMatrix" "0 -1 1 1 0 0 0 0 1"' /etc/X11/xorg.conf.d/40-libinput.conf  # 90 degrees left
 
-
-# Configure .xinitrc (using the dynamically created KIOSK_URL)
+# Configure .xinitrc
 cat << EOF > ~/.xinitrc
+# Source .bashrc to get environment variables
 . ~/.bashrc
+
 # Disable screen blanking and power saving
 xset -dpms
 xset s off
 xset s noblank
 
-xrandr --output DSI-1 --rotate left  # Replace DSI-1 if necessary
+# Rotate display
+xrandr --output DSI-1 --rotate left   # Replace DSI-1 if needed
 
-chromium-browser --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar --kiosk "\$KIOSK_URL" --check-for-update-interval=31536000 & # Note the escaped \$
+# Launch Chromium in kiosk mode
+chromium-browser --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar --kiosk "\$KIOSK_URL" --check-for-update-interval=31536000 &
 
+# Start and make openbox persist
 openbox
 exec openbox
 EOF
 
-# Configure .bash_profile to start X
-cat << EOF > ~/.bash_profile
-[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor
-EOF
-
+# Start x server on boot
+echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx --" >> ~/.bashrc
 
 # Reboot
 sudo reboot
