edf856091263e767e65551333e5a220f23f79843
Chris Kunde <chriskunde@live.com>
2024-11-13 09:59:37 -0500

    Test 5

--- log_device_setup.sh
+++ log_device_setup.sh
@@ -14,16 +14,19 @@ echo "Enabling Autologin"
 sudo raspi-config nonint do_boot_behaviour B2
 
 # Update Raspberry OS
+echo "Updating and Upgrading Firmware"
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install minimum GUI components
+echo "Installing Minimum GUI"
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 
 # Install Chromium Web browser
+echo "Intstalling Chrome"
 sudo apt-get install --no-install-recommends chromium-browser -y
 
 # Rotate Touch
-echo "Rotate Touch Input"
+echo "Rotating Touch Input"
 sudo cp /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/
 sudo sed -i '/MatchIsTouchscreen "on"/a\        Option "CalibrationMatrix" "0 -1 1 1 0 0 0 0 1"' /etc/X11/xorg.conf.d/40-libinput.conf  # 90 degrees left
 # "0 1 0 -1 0 1 0 0 1" = 90 Dregree (right)
@@ -48,13 +51,13 @@ EOF'
 
 # Set Openbox environment
 echo "Setting Openbox Environment"
-sudo bash -c `echo "export KIOSK_URL=\"${KIOSK_URL}\"" > /etc/xdg/openbox/environment`
+sudo bash -c "echo "export KIOSK_URL=\"${KIOSK_URL}\"" > /etc/xdg/openbox/environment"
 
 # Insert Start conditions to bash_profile
 echo "Setting Start Conditions"
 BASH_PROFILE_CONTENT="[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor"
 touch ~/.bash_profile
-sudo bash -c `echo "$BASH_PROFILE_CONTENT" > ~/.bash_profile`
+sudo bash -c "echo "$BASH_PROFILE_CONTENT" > ~/.bash_profile"
 
 # Source the ~/.bash_profile
 source ~/.bash_profile
