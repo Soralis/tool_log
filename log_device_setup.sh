61493603df54cf3c392f9ee578cf2e0fb3904fc2
Chris Kunde <chriskunde@live.com>
2024-11-13 07:16:40 -0500

    try4

--- log_device_setup.sh
+++ log_device_setup.sh
@@ -12,44 +12,53 @@ KIOSK_URL="${BASE_URL}?device_name=${DEVICE_NAME}"
 # Enable autologin to command line
 sudo raspi-config nonint do_boot_behaviour B2
 
-# Update and upgrade
-sudo apt update -y && sudo apt upgrade -y
+# Update Raspberry OS
+sudo apt-get update -y && sudo apt-get upgrade -y
 
-# Install necessary packages
-sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser xserver-xorg-input-libinput -y
+# Install minimum GUI components
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox xserver-xorg-input-libinput -y
 
-# Set KIOSK_URL in the environment
-echo "export KIOSK_URL=\"${KIOSK_URL}\"" > /etc/xdg/openbox/environment
+# Install Chromium Web browser
+sudo apt-get install --no-install-recommends chromium-browser
 
-
-# Configure libinput for touch rotation
-sudo mkdir -p /etc/X11/xorg.conf.d
+# Rotate Touch
+sudo mkdir /etc/X11/xorg.conf.d
 sudo cp /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/
 sudo sed -i '/MatchIsTouchscreen "on"/a\        Option "CalibrationMatrix" "0 -1 1 1 0 0 0 0 1"' /etc/X11/xorg.conf.d/40-libinput.conf  # 90 degrees left
 
-
-# --- Configure Openbox autostart ---
-cat << EOF > /etc/xdg/openbox/autostart
+# Edit Openbox config
+sudo bash -c 'cat << EOF > /etc/xdg/openbox/autostart
 # Disable screen blanking/power saving
 xset -dpms
 xset s off
 xset s noblank
 
-# Rotate Display (NEW)
-xrandr --output DSI-1 --rotate left   #Replace if necessary
+# Rotate Display
+xrandr --output DSI-1 --rotate left
 
-# Launch Chromium in kiosk mode
-chromium-browser --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar --kiosk \$KIOSK_URL --check-for-update-interval=31536000 --disable-gpu &
-EOF
+sed -i '"'"'s/"exited_cleanly":false/"exited_cleanly":true/'"'"' ~/.config/chromium/'"'"'Local State'"'"'
+sed -i '"'"'s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"["]+"/"exit_type":"Normal"/"'"'"' ~/.config/chromium/Default/Preferences
+chromium-browser --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar --kiosk \$KIOSK_URL --check-for-update-interval=31536000 &
+EOF'
 
-# --- Configure a minimal .xinitrc  ---
-cat << EOF > ~/.xinitrc
-. ~/.bashrc
-exec openbox
-EOF
+# Set Openbox environment
+sudo bash -c 'echo "export KIOSK_URL=\"${KIOSK_URL}\"" > /etc/xdg/openbox/environment'
 
-# Start x server on boot
-echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx --" >> ~/.bashrc
+# Check if bash_profile exists
+if [ -f ~/.bash_profile ]; then
+    echo "~/.bash_profile exists, editing it directly."
+    sudo bash -c 'cat << EOF >> ~/.bash_profile
+[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor
+EOF'
+else
+    echo "~/.bash_profile does not exist, creating it."
+    sudo bash -c 'cat << EOF > ~/.bash_profile
+[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor
+EOF'
+fi
 
-# Reboot
+# Source the ~/.bash_profile
+source ~/.bash_profile
+
+# Reboot the Raspberry Pi
 sudo reboot
