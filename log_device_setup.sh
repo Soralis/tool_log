46cce9b1cfaee94062c696bf44a301d3d2c145bc
Chris Kunde <chriskunde@live.com>
2024-11-14 08:01:34 -0500

    changed DSI from 1 to 0

--- log_device_setup.sh
+++ log_device_setup.sh
@@ -42,7 +42,7 @@ xset s off
 xset s noblank
 
 # Rotate Display
-xrandr --output DSI-1 --rotate left
+xrandr --output DSI-0 --rotate left
 
 sed -i '"'"'s/"exited_cleanly":false/"exited_cleanly":true/'"'"' ~/.config/chromium/'"'"'Local State'"'"'
 sed -i '"'"'s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"["]+"/"exit_type":"Normal"/"'"'"' ~/.config/chromium/Default/Preferences
