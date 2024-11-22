51dc85d98acef2009fb6236df4ab63ba9a495c6c
Chris Kunde <chriskunde@live.com>
2024-11-22 17:13:00 -0500

    x3

--- deploy.sh
+++ deploy.sh
@@ -72,7 +72,7 @@ else
 	fi
 	echo "Innovative! Commence Deploy of new Version."
     else
-    echo "Not newer than current Version. Exiting."
+    echo "Nothing newer than the current Version. Exiting."
 	exit 0
     fi
     cd "$NEW_DIR"
