1a40dad718a214e3c1431149ff849cef4273086e
Chris Kunde <chriskunde@live.com>
2025-01-07 13:23:23 -0500

    revert back to original setting

--- app/database_config.py
+++ app/database_config.py
@@ -3,10 +3,10 @@ from sqlmodel import SQLModel, Session, create_engine
 # Database configuration
 # PostgreSQL connection URL for psycopg (v3)
 # DEV DB
-# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost/tool_log_db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost/tool_log_db"
 
 # LIVE DB:
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:postgres@10.0.36.192/tool_log_db"
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:postgres@10.0.36.192/tool_log_db"
 
 # SQLAlchemy settings
 SQLALCHEMY_TRACK_MODIFICATIONS = False
