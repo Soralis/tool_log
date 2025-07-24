645e390a61de5dcde9f4e4a09e520c934f008a09
Soralis <chriskunde@live.com>
2025-07-24 15:19:41 -0400

    test 12

--- server_setup.sh
+++ server_setup.sh
@@ -8,12 +8,9 @@ sudo apt-get install -y nginx python3-venv wayfire chromium-browser
 
 # Enable console autologin and launch Wayfire on tty1
 sudo raspi-config nonint do_boot_behaviour B2
-sudo bash -c 'touch /home/pi/.bash_profile
-cat >> /home/pi/.bash_profile << EOF
-[[ -z \$WAYLAND_DISPLAY && \$XDG_VTNR -eq 1 ]] && exec wayfire
-EOF
-chown pi:pi /home/pi/.bash_profile
-chmod 644 /home/pi/.bash_profile'
+sudo -u pi touch /home/pi/.bash_profile
+sudo -u pi chmod 644 /home/pi/.bash_profile
+sudo -u pi bash -c 'echo "[[ -z \$WAYLAND_DISPLAY && \$XDG_VTNR -eq 1 ]] && exec wayfire" >> /home/pi/.bash_profile'
 
 # 2. Configure pi-user crontab for deploy and weekly reboot
 echo "Configuring crontab for check_github and weekly reboot..."
